4d3c46253bde0edd36b8c1eb637db0b3120619d7
arnold-jr <j.arnold.111@gmail.com>
2016-09-07 18:31:08 -0500

    Added gpu example directory

---
+++ gpu/gpu_eg.m
@@ -0,0 +1,4 @@
+% surrounding a block of code with tic and toc
+% will time its execution
+
+tests();
---
+++ gpu/tests.m
@@ -0,0 +1,82 @@
+function [] = tests()
+
+    test_one(10000000);
+    test_two(2000);
+    test_three(2000);
+    
+    
+    
+    function [] = test_one(n)
+
+    fprintf('testing sine function\n');
+    % host array
+    H = ones(n,1);
+
+    % device (gpu) array
+    D = gpuArray(H);
+
+    % CPU code
+    tic
+    y = sin(H);
+    time_cpu = toc;
+    fprintf('time_cpu %f\n',time_cpu)
+
+    % GPU code
+    tic
+    y = sin(D);
+    time_gpu = toc;
+    fprintf('time_gpu %f\n',time_gpu)
+
+    end
+
+    function [] = test_two(n)
+
+    fprintf('testing mldivide without copies\n');
+    % host arrays
+    A = rand(n);
+    b = rand(n,1);
+
+    % device arrays
+    A_dev = gpuArray(A);
+    b_dev = gpuArray(b); 
+
+    % CPU code using Matlab's optimized mldivide
+    tic
+    x = A\b;
+    time_cpu = toc;
+    fprintf('time_cpu %f\n',time_cpu)
+
+    % GPU code for mldivide that accepts objects
+    tic
+    x_dev = mldivide(A,b);
+    time_gpu = toc;
+    fprintf('time_gpu %f\n',time_gpu)
+
+    end
+    
+    function [] = test_three(n)
+
+    fprintf('testing mldivide with copies\n');
+
+    % device arrays
+
+    % CPU code using Matlab's optimized mldivide
+    tic
+    A = rand(n);
+    b = rand(n,1);
+    x = A\b;
+    time_cpu = toc;
+    fprintf('time_cpu %f\n',time_cpu)
+
+    % GPU code for mldivide that accepts objects
+    tic
+    A_dev = gpuArray(A);
+    b_dev = gpuArray(b); 
+    x_dev = mldivide(A,b);
+    x_host = gather(x_dev);
+    time_gpu = toc;
+    fprintf('time_gpu %f\n',time_gpu)
+
+    end
+end
+
